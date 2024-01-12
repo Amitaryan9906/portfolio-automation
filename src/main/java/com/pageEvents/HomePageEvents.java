79188eb6121346488d1ec2e03684703bd8eabd59
Amit-Das <damit5221@gmail.com>
2024-01-12 22:06:41 +0530

    Updated

--- src/main/java/com/pageEvents/HomePageEvents.java
+++ src/main/java/com/pageEvents/HomePageEvents.java
@@ -3,12 +3,16 @@ package com.pageEvents;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import com.base.BaseTest;
-
+import com.utils.ElementFetch;
+import com.pageObjects.*;
 public class HomePageEvents extends BaseTest{
+	
+	ElementFetch el = new ElementFetch();
 	
 	public void homePage() {
 		 //  Verify that the home page is displayed
         Assert.assertTrue(driver.findElement(By.id("home")).isDisplayed(), "Home page is not displayed");
+        el.getWebElement("XPATH",HomePageObjects.homeTabElementXpath).click();
 
 	}
 }
---
+++ src/main/java/com/pageObjects/HomePageObjects.java
@@ -0,0 +1,11 @@
+package com.pageObjects;
+
+public interface HomePageObjects {
+
+	String homeTabElementXpath="//a[@class='nav-link-home']";
+	String aboutTabXpath="//a[@class='nav-link-about']";
+	String skillTabXpath="//a[@class='nav-link-skill']";
+	String projectsTabXpath="//a[@class='nav-link-projects']";
+	String contactTabXpath="//a[@class='nav-link-contact']";
+	String resumeBtnXpath="//a[@class='nav-link-resume']";
+}
